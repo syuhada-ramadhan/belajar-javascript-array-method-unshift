caf4aebbf40ba2a4a8dcaf07720a26c3ba94df85
syuhada-ramadhan <suhadahada18@gmail.com>
2021-06-07 17:29:41 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// array hewan
+const hewan = ["Kambing", "Kelinci"];
+
+// cetak array hewan sebelum di tambahkan elemen baru
+console.log(hewan);
+
+// menambahkan elemen "Ayam" ke array hewan
+hewan.unshift("Ayam");
+
+// cetak array hewan setelah di tambahkan elemen baru
+console.log(hewan);
